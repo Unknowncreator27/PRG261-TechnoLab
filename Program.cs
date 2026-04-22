dee4a3a6d3d036d424c88e409bd3db3e6deb4ee1
UnknownCreator27 <steel27123@gmail.com>
2026-04-22 17:11:48 +0200

    added the contributors list

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 
 namespace PRG261_TechnoLab
 {
+
+    /// <summary>
+    /// 
+    /// Contributors
+    ///  - Hanro Lombard (603200) - Did most (or all of the massive coding)
+    ///  - Thabo Hammer - Made small changes to the code
+    ///  - Tarah Barwe - Made small changes to the code
+    /// </summary>
     public class Booking
     {
         public string StudentfName;
